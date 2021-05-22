0cfe9cf3843b53654d18924166d9670a123c873b
Szymandos <szamanburito@gmail.com>
2021-05-22 19:49:01 +0200

    h

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,12 @@ function wypisz() {
     var liczba2 = document.getElementById("drugie").value;
     var napis = "";
 
-    for (i = liczba1; i <= liczba2; i++) {
-        napis = napis + i + " ";
-
+    if (liczba2 > liczba1) document.getElementById("numery").innerHTML = Pierwsza jest za duża;
+    else(liczba2 == liczba1) document.getElementById("numery").innerHTML = "Liczby są takie same";
+    else {
+        for (i = liczba1; i <= liczba2; i++) {
+            napis = napis + i + " ";
+        }
+        document.getElementById("numery").innerHTML = napis;
     }
-    setTimeout("wypisz()", 1000);
-    document.getElementById("numery").innerHTML = napis;
-
 }
