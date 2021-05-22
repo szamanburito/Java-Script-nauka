91eb6d88f1639175f37990c6e5562a8aacd19f37
Szymandos <szamanburito@gmail.com>
2021-05-22 20:28:41 +0200

    hf

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,8 @@ function wypisz() {
     var liczba2 = document.getElementById("drugie").value;
     var napis = "";
 
-    if (liczba2 > liczba1) document.getElementById("numery").innerHTML = Pierwsza jest za duża;
-    else(liczba2 == liczba1) document.getElementById("numery").innerHTML = "Liczby są takie same";
+    if (liczba2 < liczba1) document.getElementById("numery").innerHTML = "Pierwsza jest za duża";
+    else if (liczba2 == liczba1) document.getElementById("numery").innerHTML = "Liczby są takie same";
     else {
         for (i = liczba1; i <= liczba2; i++) {
             napis = napis + i + " ";
