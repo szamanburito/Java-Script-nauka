9581cebc7a48bc8d22eb55840a4e3c1115446fbc
Szymandos <szamanburito@gmail.com>
2021-05-22 19:29:25 +0200

    hotfix

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,7 @@ function wypisz() {
 
     for (i = liczba1; i <= liczba2; i++) {
         napis = napis + i + " ";
-        setTimeout(1000)
+        setTimeout(wypisz(), 1000);
     }
     document.getElementById("numery").innerHTML = napis;
 
