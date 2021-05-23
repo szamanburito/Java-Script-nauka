6d37c84183b86ebd2c82796fcf3bf1e0f4ea8cff
Szymandos <szamanburito@gmail.com>
2021-05-24 01:30:17 +0200

    hf

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,18 @@
-function
+var numer = Math.floor(Math.random() * 4) + 1;
+
+function schowaj() {
+    $("slider").fadeOut(500);
+}
+
+function changephoto() {
+    numer++;
+    if (numer > 4) numer = 1;
+
+    var plik = "<img src=\"../../Photos/slide" + numer + ".jpg\" />";
+
+    document.getElementById("slider").innerHTML = plik;
+    $("#slider").fadeIn(500);
+
+    setTimeout("changephoto()", 5000);
+    setTimeout("schowaj()", 4500);
+} 
