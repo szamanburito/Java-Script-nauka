7ee1a68ceb9e7735f30ca3ed1abead417f16edf7
Szymandos <szamanburito@gmail.com>
2021-05-30 04:17:38 +0200

    hf

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,6 @@
-function
+var liczba = prompt("wpisz liczbe")
+
+function naszaLiczba() {
+    return (liczba >= 0) ? liczba : -liczba;
+}
+alert("Wartość bezwzględna z liczby " + liczba + " wynosi " + naszaLiczba());
