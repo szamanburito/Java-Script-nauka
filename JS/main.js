4f2a07c76bb5e4be1ef07a36033686d9748f6fe2
Szymandos <szamanburito@gmail.com>
2021-05-30 18:50:01 +0200

    hf

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,18 @@
-function
+function oblicz() {
+    do {
+        var liczba = prompt("Podaj liczbe dodatnią", "");
+        if (isNaN(liczba)) {
+            alert("to nie jest liczba");
+            liczba = -999;
+        } else if (liczba == "") {
+            alert("może byś coś napisał?");
+            liczba = -999;
+        } else if (liczba < 0) {
+            alert("dodatnią pajacu");
+        } else if (liczba = null) {
+            liczba = -999;
+        } else {}
+    } while (liczba < 0);
+    alert("brawo, " + liczba + " to liczba dodatnia");
+}
+oblicz();
